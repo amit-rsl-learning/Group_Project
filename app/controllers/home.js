c2836a3fa005ccf098642643ddb85695a4166ec9
Amit Kamat <amit.kamat@rsllearning.com>
2025-03-07 12:37:03 +0530

    Developed home page

---
+++ app/controllers/home.js
@@ -0,0 +1,23 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+export default class HomeController extends Controller {
+  @tracked displayMessage = 'Search a user to begin';
+  @tracked userFound = false;
+  @tracked userProfile = {};
+
+  @action
+  async searchUser(e) {
+    const response = await fetch(
+      `https://api.github.com/users/${e.target.value}`,
+    );
+    const data = await response.json();
+    if (response.status == 200) {
+      this.userProfile = data;
+      this.userFound = true;
+    } else {
+      this.displayMessage = 'No user found!!';
+      this.userFound = false;
+    }
+  }
+}
